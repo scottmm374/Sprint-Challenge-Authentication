a421ccd70dd5652b3167456dd0c655309fd97a1f
Michelle Scott <scottmm374@gmail.com>
2020-01-27 12:06:06 -0500

    All tests are passing

--- api/server.spec.js
+++ api/server.spec.js
@@ -24,16 +24,30 @@ describe("user registration", () => {
   });
 });
 
-// describe("user login", () => {
-//   it("should login user", async () => {
-//     const login = await supertest(server)
-//       .post("/api/auth/login")
-//       .send({
-//         username: "Ann",
-//         password: "Nothing"
-//       });
-//     expect(res.status).toBe(200);
-//     expect(login.text).toContain("token");
-//     expect(login.text).toContain("message");
-//   });
-// });
+test("should login user", async () => {
+  const login = await supertest(server)
+    .post("/api/auth/login")
+    .send({
+      username: "Jason",
+      password: "Nickleback"
+    });
+  expect(login.status).toBe(200);
+  expect(login.text).toContain("token");
+  expect(login.text).toContain("message");
+});
+
+// PASS  api/server.spec.js
+// PASS  api/server.spec.js
+//  √ server route /  (25ms)
+//  √ should login user (103ms)
+//  user registration
+//    √ should reg new user (514ms)
+
+//  console.log auth/auth-router.js:22
+//    username, password Jason Nickleback
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        4.549s
+// Ran all test suites related to changed files.
